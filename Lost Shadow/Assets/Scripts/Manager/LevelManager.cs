f1e038b8e7d95206b038a6a2db935c9e464a56f5
_Yusaki <36989640+Speedspencer@users.noreply.github.com>
2022-03-21 00:03:11 +0700

    Revert "Merge branch 'New-Merge' into Light-Room"
    This reverts commit beec1c8aae2dd3534c1ffdb8ab2b660fb7e16218, reversing
    changes made to cfc4ebec3d9ffdd296e85ff2a5ac7581ee5980e0.

--- Lost Shadow/Assets/Scripts/Manager/LevelManager.cs
+++ Lost Shadow/Assets/Scripts/Manager/LevelManager.cs
@@ -1,4 +1,4 @@
-using Old.Controller;
+using System;
 using UnityEngine;
 
 namespace Manager
@@ -8,88 +8,30 @@ namespace Manager
         #region Variables
         [SerializeField] private Transform startPos;
         [SerializeField] private GameObject playerPrefab;
-        [SerializeField] private GameObject playerClonePrefab;
         [SerializeField] private GameObject cameraPrefab;
-        [SerializeField] private PlayerController playerController;
-
-        private GameObject _player;
-        private GameObject _playerClone;
+    
+        private GameObject _player; 
         private GameObject _camera;
-        private GameObject _overlayCamera;
 
         #endregion
 
-        public static LevelManager Instance;
-    private void Awake()
-    {
-        if (Instance == null)
+        private void Start()
         {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(this);
-            return;
-        }
-
-        DontDestroyOnLoad(gameObject);
-    }
-    
-    private void Start()
-    {
-        _player = GameObject.FindWithTag("Player");
-        _camera = GameObject.FindWithTag("Camera");
-        _overlayCamera = GameObject.FindWithTag("OverlayCamera");
-        _camera = GameObject.FindWithTag("MainCamera");
-        startPos = GameObject.Find("StartPos").transform;
-        if (_player == null) {
-            Instantiate(playerPrefab, startPos.position, startPos.rotation);
-            Instantiate(playerClonePrefab, startPos.position, startPos.rotation);
-            _playerClone = GameObject.FindWithTag("PlayerClone");
             _player = GameObject.FindWithTag("Player");
-            playerController = _player.GetComponent<PlayerController>();
-            //StartCutscene();
-        }
+            _camera = GameObject.FindWithTag("Camera");
+            startPos = GameObject.Find("StartPos").transform;
+            if (_player == null) {
+                Instantiate(playerPrefab, startPos.position, startPos.rotation);
+                GameSaveManager.Instance.SaveGame();
+
+            }
             
-        if (_camera == null)
-        {
-            Instantiate(cameraPrefab, startPos.position, startPos.rotation);
-            _camera = GameObject.FindWithTag("MainCamera");
-            _overlayCamera = GameObject.FindWithTag("OverlayCamera");
+            if (_camera == null)
+            {
+                Instantiate(cameraPrefab, startPos.position, startPos.rotation);
+            }
         }
-        GameSaveManager.Instance.SaveGame();
         
-        
-        
-    }
-
-    private void Update()
-    {
-        FollowMain();
-    }
-
-
-    void FollowMain()
-    {
-        if (playerController.isShadow)
-        {
-            _overlayCamera.transform.position = _camera.transform.position - new Vector3(0, 100);
-            _playerClone.transform.position = _player.transform.position - new Vector3(0, 100);
-        }
-        else
-        {
-            _overlayCamera.transform.position = _camera.transform.position + new Vector3(0, 100);
-            _playerClone.transform.position = _player.transform.position + new Vector3(0, 100);
-        }
-        playerController.peek.transform.position = _player.transform.position;
-        _overlayCamera.GetComponent<Camera>().orthographic = _camera.GetComponent<Camera>().orthographic;
-    }
-    
-
-    /*Void StartCutscene()
-        {
-            
-        }*/
         
     }
 }
