373796767ca8f475419202ae461b134bc56b6401
FrozenTEAR <yotsaphat.laph@bumail.net>
2022-03-22 20:01:55 +0700

    Add sprites
    -ChainEye model
    -Key model

--- Lost Shadow/Assets/Scripts/GoldKey.cs
+++ Lost Shadow/Assets/Scripts/GoldKey.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class GoldKey : MonoBehaviour
 {
     [SerializeField] GameObject goldBlock;
     private SpriteRenderer _blockSR;
     private BoxCollider2D _blockBC2D;
+    private Animator _blockAnimator;
+    private int _count = 0;
     void Start()
     {
         _blockSR = goldBlock.GetComponent<SpriteRenderer>();
         _blockBC2D = goldBlock.GetComponent<BoxCollider2D>();
+        _blockAnimator = goldBlock.GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        if (_blockAnimator.GetBool("Broken") != false)
+        {
+            _count += 1;
+            if (_count > 14)
+            {
+                Destroy(goldBlock.gameObject);
+            }
+        }
+    }
+    
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            _blockSR.enabled = false;
+            _blockAnimator.SetBool("Broken", true);
             _blockBC2D.enabled = false;
             Destroy(this.gameObject);
         }
--- Lost Shadow/Assets/Scripts/SilverKey.cs
+++ Lost Shadow/Assets/Scripts/SilverKey.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class SilverKey : MonoBehaviour
 {
     [SerializeField] GameObject silverBlock;
     private SpriteRenderer _blockSR;
     private BoxCollider2D _blockBC2D;
+    private Animator _blockAnimator;
+    private int _count = 0;
     void Start()
     {
         _blockSR = silverBlock.GetComponent<SpriteRenderer>();
         _blockBC2D = silverBlock.GetComponent<BoxCollider2D>();
+        _blockAnimator = silverBlock.GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        if (_blockAnimator.GetBool("Broken") != false)
+        {
+            _count += 1;
+            if (_count > 14)
+            {
+                Destroy(silverBlock.gameObject);
+            }
+        }
+    }
+    
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            _blockSR.enabled = false;
+            _blockAnimator.SetBool("Broken", true);
             _blockBC2D.enabled = false;
             Destroy(this.gameObject);
         }
